c2425a00dd08e738361f62a99d8177c4ef4014e4
louohr <louise.ohrner@gmail.com>
2024-09-02 14:00:26 +0200

    fix duplicate nav links

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { Outlet } from "react-router-dom";
 export function App() {
   return (
     <div>
-      <nav className="navbar">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-      </nav>
       <Outlet /> {/* This is where child routes will render */}
     </div>
   );
